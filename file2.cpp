1074443d1b565351c513d577ad21a7d366c87611
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-03-18 20:13:35 +0700

    Do while

--- file2.cpp
+++ file2.cpp
@@ -22,4 +22,15 @@ int main() {
 
     cout << "nilai x terakhir = " << x << endl;
     cout << "jumlah perulangan =" << perulanganwhile << endl;
+
+    //perulangan do while
+    cout << endl;
+    cout << "perulangan do while" << endl; 
+    do{
+        cout << " nilai x = " << x << " endl";
+        x =  rand() % 10;
+        perulangando++;
+    }while(x < 7);
+    cout << "Nilai x terakhir = " << x << endl;
+    cout << "Jumlah perulangan do while= " << perulangando << endl;
 }
