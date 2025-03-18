82e86319d36529dd6abc71134e6d3d6bc79531a9
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-03-18 18:56:02 +0700

    pembuatan program pengulangan for

---
+++ file1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() { 
+    //pencacah naik
+    int i;
+    for (i = 0; i <5; i++){
+        cout << "Nilai i =" "<< i <<" << endl; 
+
+    }
+    cout << "Nilai i terakhir : " << i << endl;
+
+    //pencacah turun 
+    for (i = 5 ; i > 0; i--){
+        cout << "nilai i = " << i << "Lari keliling lapangan" << endl;
+    }
+    cout << "Nilai i Terakhir : " << i << endl;
+}
