0b1b1a0d618929acbbcf533a22b8b7d7071a8089
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-03-18 20:19:26 +0700

    Tanpa do while

--- file2.cpp
+++ file2.cpp
@@ -23,14 +23,4 @@ int main() {
     cout << "nilai x terakhir = " << x << endl;
     cout << "jumlah perulangan =" << perulanganwhile << endl;
 
-    //perulangan do while
-    cout << endl;
-    cout << "perulangan do while" << endl; 
-    do{
-        cout << " nilai x = " << x << " endl";
-        x =  rand() % 10;
-        perulangando++;
-    }while(x < 7);
-    cout << "Nilai x terakhir = " << x << endl;
-    cout << "Jumlah perulangan do while= " << perulangando << endl;
 }
