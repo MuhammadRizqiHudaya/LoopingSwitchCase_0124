5d9bf2b0ca9e831b50b7dfe5bab952139f8a60f2
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-03-18 20:25:59 +0700

    tanpa cacah turun

--- file1.cpp
+++ file1.cpp
@@ -9,10 +9,4 @@ int main() {
 
     }
     cout << "Nilai i terakhir : " << i << endl;
-
-    //pencacah turun 
-    for (i = 5 ; i > 0; i--){
-        cout << "nilai i = " << i << "Lari keliling lapangan" << endl;
-    }
-    cout << "Nilai i Terakhir : " << i << endl;
 }
